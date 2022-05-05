5dfa6f75aa4f56781b0e11a63320c249a8c302f7
zarjitsky <brunozar@poli.ufrj.br>
2022-05-05 20:35:26 -0300

    Versão final do lab3

---
+++ README.md
@@ -0,0 +1,39 @@
+# Computação Concorrente 2022.1
+*Bruno Zarjitsky*
+*115117384*
+
+***
+
+## LAB 3
+
+*Objetivo:* Implementar uma solução sequencial e uma concorrente para o problema de encontrar o *menor* e o *maior* valor de um vetor de números reais.
+
+
+### 10^5
+
+Numero de threads | Tempo sequencial | Tempo concorrente | Desempenho
+----------------- |------------------|-------------------|------------
+1 | 0.000433 | 0.000930 | 0.465365
+2 | 0.001329 | 0.000522 | 2.547926
+4 | 0.001528 | 0.001259 | 1.213483
+
+***
+
+### 10^7
+
+Numero de threads | Tempo sequencial | Tempo concorrente | Desempenho
+----------------- |------------------|-------------------|------------
+1 | 0.036949 | 0.039273 | 0.940815
+2 | 0.035819 | 0.018898 | 1.895408
+4 | 0.039782 | 0.018458 | 2.155214
+
+***
+
+### 10^8
+
+Numero de threads | Tempo sequencial | Tempo concorrente | Desempenho
+----------------- |------------------|-------------------|------------
+1 | 0.366762 | 0.360898 | 1.016249
+2 | 0.373755 | 0.182388 | 2.049229
+4 | 0.360600 | 0.130908 | 2.754598
+
--- labs/lab3.c
+++ labs/lab3.c
@@ -11,67 +11,158 @@ Acha o menor e o maior valor de um vetor
 #include"timer.h"
 #include<time.h>
 
+// Variaveis globais
+float *vetor;
 
-float *vetor; // Vetor de entrada
-int nthreads; // Numero de threads
-// float maior; // Maior numero do vetor
-// float menor; // Menor numero do vetor
+typedef struct {
+    int id;
+    int dimensao_do_vetor;
+    int tamanho_passo;
+    float maior_valor;
+    float menor_valor;
+} MaiorMenorValor;
 
-void * tarefa(void * arg){
-    
+// Funcao achaMaiorMenorValor
+/* 
+Recebendo um objeto do tipo MaiorMenorValor acha o menor e o maior valor de um pedaco do vetor 
+*/
+void achaMaiorMenorValor(MaiorMenorValor *arg){
+    for (int i = arg->id; i < arg->dimensao_do_vetor; i += arg->tamanho_passo){
+        if (i == arg->id){
+            arg->maior_valor = vetor[i];
+            arg->menor_valor = vetor[i];
+        }
+        if (vetor[i] < arg->menor_valor){
+            arg->menor_valor = vetor[i];
+        }
+        if (vetor[i] > arg->maior_valor){
+            arg->maior_valor = vetor[i];
+        }
+    }
 }
 
 
+void * funcaoConcorrente(void * arg){
+    MaiorMenorValor *maior_e_menor = (MaiorMenorValor*) arg;
+    achaMaiorMenorValor(maior_e_menor);
+    pthread_exit(NULL);
+}
+
+// Funcao para imprimir o vetor
+void imprime_vetor(long long int dimensao_do_vetor){
+    int dimensao_maxima = 20;
+    printf("\nVetor de entrada: \n");
+    // Evita de ficar mostrando muitos valores
+    if (dimensao_do_vetor > dimensao_maxima){
+        dimensao_do_vetor = dimensao_maxima;
+    }
+    for (int i = 0; i < dimensao_do_vetor; i++){
+        printf("%.2f ", vetor[i]);
+    }
+    printf("\n\n");
+}
 
 int main(int argc, char *argv[]){
-    // Definecao de variaveis
-    long int dim;
-    int nthreads;
-    float menor, maior;
 
-    // Iniciacao de valores
-    srand(time(0));   // Inicia a lib do rand()
+    // Definicao de variaveis
+    int mostra_vetor = 0;
+    int numero_de_threads;
+    long long int dimensao_do_vetor;
+    double inicio;
+    double fim;
+    double tempo_concorrente;
+    double tempo_sequencial;
+    double desempenho;
+    pthread_t *id_thread;
+    MaiorMenorValor maior_menor_sequencial;
+    MaiorMenorValor maior_menor_concorrente;
+    MaiorMenorValor *maior_menor_thread;
+
+    // Inicia a lib do rand()
+    srand(time(0));
 
     // Verificacao da entrada do programa
     if (argc<3){
         fprintf(stderr, "Digite %s <dimesao do vetor> <numero de threads>\n", argv[0]);
         return 1;
     }
-    dim = atoi(argv[1]);
-    nthreads = atoi(argv[2]);
+    dimensao_do_vetor = atoi(argv[1]);
+    numero_de_threads = atoi(argv[2]);
 
     // Alocacao de memoria
-    vetor = (float*) malloc(sizeof(float) * dim);
-    if (vetor == NULL){
+    vetor = (float*) malloc(sizeof(float) * dimensao_do_vetor);
+    id_thread = (pthread_t*) malloc(sizeof(pthread_t) * numero_de_threads);
+    maior_menor_thread = (MaiorMenorValor*) malloc(sizeof(MaiorMenorValor) * numero_de_threads);
+    if (vetor == NULL || id_thread == NULL || maior_menor_thread == NULL){
         fprintf(stderr, "ERRO--malloc\n");
     }
 
     // Inicializacao do vetor
-    for (int i = 0; i < dim; i++){
-        vetor[i] = ((rand() % 100) * 1.1)/(i + 1);
-        if (i == 0){
-            maior = vetor[i];
-            menor = vetor[i];
-        }
+    for (int i = 0; i < dimensao_do_vetor; i++){
+        vetor[i] = rand() * 1.1;
+    }
+
+    // Mostra o vetor se necessario
+    if (mostra_vetor == 1){
+        imprime_vetor(dimensao_do_vetor);
     }
 
     // Parte sequencial
-    for (int i = 0; i < dim; i++){
-        if (vetor[i] < menor){
-            menor = vetor[i];
-        }
-        if (vetor[i] > maior){
-            maior = vetor[i];
+    GET_TIME(inicio);
+    maior_menor_sequencial.dimensao_do_vetor = dimensao_do_vetor;
+    maior_menor_sequencial.id = 0;
+    maior_menor_sequencial.tamanho_passo = 1;
+
+    achaMaiorMenorValor(&maior_menor_sequencial);
+    GET_TIME(fim);
+    tempo_sequencial = fim-inicio;
+
+    // Parte concorrente
+    GET_TIME(inicio);
+
+    for (int i = 0; i < numero_de_threads; i++){
+        (maior_menor_thread + i)->id = i;
+        (maior_menor_thread + i)->dimensao_do_vetor = dimensao_do_vetor;
+        (maior_menor_thread + i)->tamanho_passo = numero_de_threads;
+        // cria as threads chamando a funcaoConcorrente
+        if (pthread_create(id_thread+i, NULL, funcaoConcorrente, (void*) (maior_menor_thread + i))){
+            puts("ERRO--pthread_create\n");
+            break;
         }
     }
 
-    // Parte concorrente
+    // Define um valor inicial para as posicoes maior_valor e menor_valor do objeto maior_menor_concorrente
+    maior_menor_concorrente.maior_valor = vetor[0];
+    maior_menor_concorrente.menor_valor = vetor[0];
+
+    // Espera as threads terminarem de executar e compara os valores
+    for (int i = 0; i < numero_de_threads; i++){
+        pthread_join(*(id_thread + i), NULL);
+        if ((maior_menor_thread + i)->maior_valor > maior_menor_concorrente.maior_valor){
+            maior_menor_concorrente.maior_valor = (maior_menor_thread + i)->maior_valor;
+        }
+        if ((maior_menor_thread + i)->menor_valor < maior_menor_concorrente.menor_valor){
+            maior_menor_concorrente.menor_valor = (maior_menor_thread + i)->menor_valor;
+        }
+    }
+
+    GET_TIME(fim);
+    tempo_concorrente = fim-inicio;
 
     // Exibicao de resultados
-    printf("Menor numero do vetor: %f\n", menor);
-    printf("Maior numero do vetor: %f\n", maior);
+    printf("Menor numero do vetor (sequencial) : %.2f\n", maior_menor_sequencial.menor_valor);
+    printf("Menor numero do vetor (concorrente): %.2f\n", maior_menor_concorrente.menor_valor);
+    printf("Maior numero do vetor (sequencial) : %.2f\n", maior_menor_sequencial.maior_valor);
+    printf("Maior numero do vetor (concorrente): %.2f\n", maior_menor_concorrente.maior_valor);
+
+    // Calcula o desempenho e exibe os tempos
+    desempenho = tempo_sequencial / tempo_concorrente;
+    printf("\n%lf / %lf\n%lf\n", tempo_sequencial, tempo_concorrente, desempenho);
 
     // Liberacao de memoria
+    free(vetor);
+    free(maior_menor_thread);
+    free(id_thread);
 
     return 0;
 }
